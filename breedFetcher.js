51be17e1404c23bec0f1b2d0e4d3121614c82567
Kyle McParland <kylemcparland@gmail.com>
2024-07-14 14:43:47 -0400

    updated breedFetcher.js to no longer use promises for the sake of the following exercise

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,46 @@
 const needle = require("needle");
 const breed = process.argv[2];
 
-needle("get", ("https://api.thecatapi.com/v1/breeds/search?q=" + breed))
-  .then((response) => {
-    if (response.body[0]) {
-      console.log(response.body[0].description);
-    } else {
-      throw new Error("Breed not found.");
-    }
-  })
-  .catch((error) => {
+needle.get(("https://api.thecatapi.com/v1/breeds/search?q=" + breed), function (error, response) {
+  // Check if error:
+  if (error) {
+    // Check if URL is correct:
     if (error.code === "ENOTFOUND") {
-      console.log("Invalid URL");
-    } else if (error.message === "Breed not found.") {
-      console.log("Breed not found.");
+      console.log("Incorrect URL.");
+      return;
     } else {
+      // Return generic error otherwise:
       console.log(error);
+      return;
     }
-  });
+  }
+  
+  // Check if breed exists in repo
+  if (response.body.length === 0) {
+    console.log("Breed not found.");
+    return;
+  }
+
+  // Otherwise, return description:
+  console.log(response.body[0].description);
+});
+
+
+
+// needle("get", ("https://api.thecatapi.com/v1/breeds/search?q=" + breed))
+//   .then((response) => {
+//     if (response.body[0]) {
+//       console.log(response.body[0].description);
+//     } else {
+//       throw new Error("Breed not found.");
+//     }
+//   })
+//   .catch((error) => {
+//     if (error.code === "ENOTFOUND") {
+//       console.log("Invalid URL");
+//     } else if (error.message === "Breed not found.") {
+//       console.log("Breed not found.");
+//     } else {
+//       console.log(error);
+//     }
+//   });
